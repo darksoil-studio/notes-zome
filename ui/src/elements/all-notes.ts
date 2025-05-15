737dee40d4b6438dd87807e7801b9e7e638a1e83
guillem.cordoba <guillem.cordoba@gmail.com>
2025-05-15 15:42:48 +0200

    Format

--- ui/src/elements/all-notes.ts
+++ ui/src/elements/all-notes.ts
@@ -1,68 +1,77 @@
-import { hashProperty, sharedStyles, wrapPathInSvg } from "@darksoil-studio/holochain-elements";
-import { SignalWatcher } from "@darksoil-studio/holochain-signals";
-import { ActionHash, AgentPubKey, EntryHash, Record } from "@holochain/client";
-import { consume } from "@lit/context";
-import { localized, msg } from "@lit/localize";
-import { mdiInformationOutline } from "@mdi/js";
-import { html, LitElement } from "lit";
-import { customElement, property, state } from "lit/decorators.js";
+import {
+	hashProperty,
+	sharedStyles,
+	wrapPathInSvg,
+} from '@darksoil-studio/holochain-elements';
+import '@darksoil-studio/holochain-elements/dist/elements/display-error.js';
+import { SignalWatcher } from '@darksoil-studio/holochain-signals';
+import { ActionHash, AgentPubKey, EntryHash, Record } from '@holochain/client';
+import { consume } from '@lit/context';
+import { localized, msg } from '@lit/localize';
+import { mdiInformationOutline } from '@mdi/js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/spinner/spinner.js';
+import { LitElement, html } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 
-import "@darksoil-studio/holochain-elements/dist/elements/display-error.js";
-import "@shoelace-style/shoelace/dist/components/spinner/spinner.js";
-import "@shoelace-style/shoelace/dist/components/icon/icon.js";
-
-import "./note-summary.js";
-import { notesStoreContext } from "../context.js";
-import { NotesStore } from "../notes-store.js";
+import { notesStoreContext } from '../context.js';
+import { NotesStore } from '../notes-store.js';
+import './note-summary.js';
 
 /**
  * @element all-notes
  */
 @localized()
-@customElement("all-notes")
+@customElement('all-notes')
 export class AllNotes extends SignalWatcher(LitElement) {
-  /**
-   * @internal
-   */
-  @consume({ context: notesStoreContext, subscribe: true })
-  notesStore!: NotesStore;
+	/**
+	 * @internal
+	 */
+	@consume({ context: notesStoreContext, subscribe: true })
+	notesStore!: NotesStore;
 
-  renderList(hashes: Array<ActionHash>) {
-    if (hashes.length === 0) {
-      return html` <div class="column placeholder center-content" style="gap: 8px; flex: 1">
-        <sl-icon
-          .src=${wrapPathInSvg(mdiInformationOutline)}
-          style="font-size: 64px;"
-          ></sl-icon
-        >
-        <span style="text-align: center">${msg("No notes found.")}</span>
-      </div>`;
-    }
+	renderList(hashes: Array<ActionHash>) {
+		if (hashes.length === 0) {
+			return html` <div
+				class="column placeholder center-content"
+				style="gap: 8px; flex: 1"
+			>
+				<sl-icon
+					.src=${wrapPathInSvg(mdiInformationOutline)}
+					style="font-size: 64px;"
+				></sl-icon>
+				<span style="text-align: center">${msg('No notes found.')}</span>
+			</div>`;
+		}
 
-    return html`
-      <div class="column" style="gap: 8px; flex: 1">
-        ${hashes.map(hash => html`<note-summary .noteHash=${hash}></note-summary>`)}
-      </div>
-    `;
-  }
+		return html`
+			<div class="column" style="gap: 8px; flex: 1">
+				${hashes.map(
+					hash => html`<note-summary .noteHash=${hash}></note-summary>`,
+				)}
+			</div>
+		`;
+	}
 
-  render() {
-    const map = this.notesStore.allNotes.get();
+	render() {
+		const map = this.notesStore.allNotes.get();
 
-    switch (map.status) {
-      case "pending":
-        return html`<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; flex: 1;">
-          <sl-spinner style="font-size: 2rem;"></sl-spinner>
-        </div>`;
-      case "error":
-        return html`<display-error
-          .headline=${msg("Error fetching the notes")}
-          .error=${map.error}
-        ></display-error>`;
-      case "completed":
-        return this.renderList(Array.from(map.value.keys()));
-    }
-  }
+		switch (map.status) {
+			case 'pending':
+				return html`<div
+					style="display: flex; flex-direction: column; align-items: center; justify-content: center; flex: 1;"
+				>
+					<sl-spinner style="font-size: 2rem;"></sl-spinner>
+				</div>`;
+			case 'error':
+				return html`<display-error
+					.headline=${msg('Error fetching the notes')}
+					.error=${map.error}
+				></display-error>`;
+			case 'completed':
+				return this.renderList(Array.from(map.value.keys()));
+		}
+	}
 
-  static styles = [sharedStyles];
+	static styles = [sharedStyles];
 }
--- ui/src/elements/create-note.ts
+++ ui/src/elements/create-note.ts
@@ -1,111 +1,113 @@
 import {
-  hashProperty,
-  hashState,
-  notifyError,
-  onSubmit,
-  sharedStyles,
-  wrapPathInSvg,
-} from "@darksoil-studio/holochain-elements";
-import { SignalWatcher } from "@darksoil-studio/holochain-signals";
-import { EntryRecord } from "@darksoil-studio/holochain-utils";
-import { ActionHash, AgentPubKey, DnaHash, EntryHash, Record } from "@holochain/client";
-import { consume } from "@lit/context";
-import { localized, msg } from "@lit/localize";
-import { mdiAlertCircleOutline, mdiDelete } from "@mdi/js";
-import { html, LitElement } from "lit";
-import { customElement, property, query, state } from "lit/decorators.js";
-import { repeat } from "lit/directives/repeat.js";
+	hashProperty,
+	hashState,
+	notifyError,
+	onSubmit,
+	sharedStyles,
+	wrapPathInSvg,
+} from '@darksoil-studio/holochain-elements';
+import '@darksoil-studio/holochain-elements/dist/elements/display-error.js';
+import { SignalWatcher } from '@darksoil-studio/holochain-signals';
+import { EntryRecord } from '@darksoil-studio/holochain-utils';
+import {
+	ActionHash,
+	AgentPubKey,
+	DnaHash,
+	EntryHash,
+	Record,
+} from '@holochain/client';
+import { consume } from '@lit/context';
+import { localized, msg } from '@lit/localize';
+import { mdiAlertCircleOutline, mdiDelete } from '@mdi/js';
+import '@shoelace-style/shoelace/dist/components/alert/alert.js';
+import SlAlert from '@shoelace-style/shoelace/dist/components/alert/alert.js';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/card/card.js';
+import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
+import { LitElement, html } from 'lit';
+import { customElement, property, query, state } from 'lit/decorators.js';
+import { repeat } from 'lit/directives/repeat.js';
 
-import "@shoelace-style/shoelace/dist/components/button/button.js";
-import "@darksoil-studio/holochain-elements/dist/elements/display-error.js";
-import "@shoelace-style/shoelace/dist/components/icon-button/icon-button.js";
-import "@shoelace-style/shoelace/dist/components/textarea/textarea.js";
-import "@shoelace-style/shoelace/dist/components/input/input.js";
-import "@shoelace-style/shoelace/dist/components/alert/alert.js";
-import "@shoelace-style/shoelace/dist/components/icon/icon.js";
-import SlAlert from "@shoelace-style/shoelace/dist/components/alert/alert.js";
-import "@shoelace-style/shoelace/dist/components/card/card.js";
-
-import { notesStoreContext } from "../context.js";
-import { NotesStore } from "../notes-store.js";
-import { Note } from "../types.js";
+import { notesStoreContext } from '../context.js';
+import { NotesStore } from '../notes-store.js';
+import { Note } from '../types.js';
 
 /**
  * @element create-note
  * @fires note-created: detail will contain { noteHash }
  */
 @localized()
-@customElement("create-note")
+@customElement('create-note')
 export class CreateNote extends SignalWatcher(LitElement) {
-  /**
-   * @internal
-   */
-  @consume({ context: notesStoreContext, subscribe: true })
-  notesStore!: NotesStore;
+	/**
+	 * @internal
+	 */
+	@consume({ context: notesStoreContext, subscribe: true })
+	notesStore!: NotesStore;
 
-  /**
-   * @internal
-   */
-  @state()
-  committing = false;
+	/**
+	 * @internal
+	 */
+	@state()
+	committing = false;
 
-  /**
-   * @internal
-   */
-  @query("#create-form")
-  form!: HTMLFormElement;
+	/**
+	 * @internal
+	 */
+	@query('#create-form')
+	form!: HTMLFormElement;
 
-  async createNote(fields: Partial<Note>) {
-    const note: Note = {
-      title: fields.title!,
-      body: fields.body!,
-    };
+	async createNote(fields: Partial<Note>) {
+		const note: Note = {
+			title: fields.title!,
+			body: fields.body!,
+		};
 
-    try {
-      this.committing = true;
-      const record: EntryRecord<Note> = await this.notesStore.client.createNote(note);
+		try {
+			this.committing = true;
+			const record: EntryRecord<Note> =
+				await this.notesStore.client.createNote(note);
 
-      this.dispatchEvent(
-        new CustomEvent("note-created", {
-          composed: true,
-          bubbles: true,
-          detail: {
-            noteHash: record.actionHash,
-          },
-        }),
-      );
+			this.dispatchEvent(
+				new CustomEvent('note-created', {
+					composed: true,
+					bubbles: true,
+					detail: {
+						noteHash: record.actionHash,
+					},
+				}),
+			);
 
-      this.form.reset();
-    } catch (e: unknown) {
-      console.error(e);
-      notifyError(msg("Error creating the note"));
-    }
-    this.committing = false;
-  }
+			this.form.reset();
+		} catch (e: unknown) {
+			console.error(e);
+			notifyError(msg('Error creating the note'));
+		}
+		this.committing = false;
+	}
 
-  render() {
-    return html`
-      <sl-card style="flex: 1;">
+	render() {
+		return html` <sl-card style="flex: 1;">
+			<form
+				id="create-form"
+				class="column"
+				style="flex: 1; gap: 16px;"
+				${onSubmit(fields => this.createNote(fields))}
+			>
+				<span class="title">${msg('Create Note')}</span>
 
-        <form 
-          id="create-form"
-          class="column"
-          style="flex: 1; gap: 16px;"
-          ${onSubmit(fields => this.createNote(fields))}
-        >  
-          <span class="title">${msg("Create Note")}</span>
+				<sl-input name="title" .label=${msg('Title')} required></sl-input>
+				<sl-textarea name="body" .label=${msg('Body')} required></sl-textarea>
 
-          <sl-input name="title" .label=${msg("Title")}  required></sl-input>
-          <sl-textarea name="body" .label=${msg("Body")}  required></sl-textarea>
+				<sl-button variant="primary" type="submit" .loading=${this.committing}
+					>${msg('Create Note')}</sl-button
+				>
+			</form>
+		</sl-card>`;
+	}
 
-          <sl-button
-            variant="primary"
-            type="submit"
-            .loading=${this.committing}
-          >${msg("Create Note")}</sl-button>
-        </form> 
-      </sl-card>`;
-  }
-
-  static styles = [sharedStyles];
+	static styles = [sharedStyles];
 }
