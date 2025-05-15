92fbe0e9627df5155a9c914877e248912887800b
guillem.cordoba <guillem.cordoba@gmail.com>
2025-05-15 15:36:28 +0200

    Fixed tests, package dependency

--- tests/src/note.test.ts
+++ tests/src/note.test.ts
@@ -1,118 +1,139 @@
-import { assert, test } from "vitest";
+import { next as Automerge } from '@automerge/automerge';
+import { toPromise } from '@darksoil-studio/holochain-signals';
+import { EntryRecord } from '@darksoil-studio/holochain-utils';
+import { cleanNodeDecoding } from '@darksoil-studio/holochain-utils/dist/clean-node-decoding.js';
+import {
+	ActionHash,
+	Delete,
+	Record,
+	SignedActionHashed,
+} from '@holochain/client';
+import { dhtSync, runScenario } from '@holochain/tryorama';
+import { decode } from '@msgpack/msgpack';
+import { assert, test } from 'vitest';
 
-import { toPromise } from "@darksoil-studio/holochain-signals";
-import { EntryRecord } from "@darksoil-studio/holochain-utils";
-import { cleanNodeDecoding } from "@darksoil-studio/holochain-utils/dist/clean-node-decoding.js";
-import { ActionHash, Delete, Record, SignedActionHashed } from "@holochain/client";
-import { dhtSync, runScenario } from "@holochain/tryorama";
-import { decode } from "@msgpack/msgpack";
+import { sampleNote } from '../../ui/src/mocks.js';
+import { Note } from '../../ui/src/types.js';
+import { setup } from './setup.js';
 
-import { sampleNote } from "../../ui/src/mocks.js";
-import { Note } from "../../ui/src/types.js";
-import { setup } from "./setup.js";
+test('create Note', async () => {
+	await runScenario(async scenario => {
+		const [alice, bob] = await setup(scenario);
 
-test("create Note", async () => {
-  await runScenario(async scenario => {
-    const [alice, bob] = await setup(scenario);
-
-    // Alice creates a Note
-    const note: EntryRecord<Note> = await alice.store.client.createNote(await sampleNote(alice.store.client));
-    assert.ok(note);
-  });
+		// Alice creates a Note
+		const note: EntryRecord<Note> = await alice.store.client.createNote(
+			await sampleNote(alice.store.client),
+		);
+		assert.ok(note);
+	});
 });
 
-test("create and read Note", async () => {
-  await runScenario(async scenario => {
-    const [alice, bob] = await setup(scenario);
+test('create and read Note', async () => {
+	await runScenario(async scenario => {
+		const [alice, bob] = await setup(scenario);
 
-    const sample = await sampleNote(alice.store.client);
+		const sample = await sampleNote(alice.store.client);
 
-    // Alice creates a Note
-    const note: EntryRecord<Note> = await alice.store.client.createNote(sample);
-    assert.ok(note);
+		// Alice creates a Note
+		const note: EntryRecord<Note> = await alice.store.client.createNote(sample);
+		assert.ok(note);
 
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0],
-    );
+		// Wait for the created entry to be propagated to the other node.
+		await dhtSync([alice.player, bob.player], alice.player.cells[0].cell_id[0]);
 
-    // Bob gets the created Note
-    const createReadOutput: EntryRecord<Note> = await toPromise(bob.store.notes.get(note.actionHash).original);
-    assert.deepEqual(sample, cleanNodeDecoding(createReadOutput.entry));
-  });
+		// Bob gets the created Note
+		const createReadOutput: EntryRecord<Note> = await toPromise(
+			bob.store.notes.get(note.actionHash).original,
+		);
+		assert.deepEqual(sample, cleanNodeDecoding(createReadOutput.entry));
+	});
 });
 
-test("create and update Note", async () => {
-  await runScenario(async scenario => {
-    const [alice, bob] = await setup(scenario);
+test('create and update Note', async () => {
+	await runScenario(async scenario => {
+		const [alice, bob] = await setup(scenario);
 
-    // Alice creates a Note
-    const note: EntryRecord<Note> = await alice.store.client.createNote(await sampleNote(alice.store.client));
-    assert.ok(note);
+		// Alice creates a Note
+		const note: EntryRecord<Note> = await alice.store.client.createNote(
+			await sampleNote(alice.store.client),
+		);
+		assert.ok(note);
 
-    const originalActionHash = note.actionHash;
+		const originalActionHash = note.actionHash;
 
-    // Alice updates the Note
-    let contentUpdate = await sampleNote(alice.store.client);
+		const latestVersion = await toPromise(
+			bob.store.notes.get(originalActionHash).latestVersion,
+		);
 
-    let updatedNote: EntryRecord<Note> = await alice.store.client.updateNote(
-      originalActionHash,
-      originalActionHash,
-      contentUpdate,
-    );
-    assert.ok(updatedNote);
+		// Alice updates the Note
+		let contentUpdate = Automerge.change<Note>(latestVersion.doc, doc => {
+			doc.title = 'hey';
+		});
 
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0],
-    );
+		let updatedNote: EntryRecord<Note> = await alice.store.client.updateNote(
+			originalActionHash,
+			[originalActionHash],
+			Automerge.save(contentUpdate),
+		);
+		assert.ok(updatedNote);
 
-    // Bob gets the updated Note
-    const readUpdatedOutput0: EntryRecord<Note> = await toPromise(bob.store.notes.get(note.actionHash).latestVersion);
-    assert.deepEqual(contentUpdate, cleanNodeDecoding(readUpdatedOutput0.entry));
+		// Wait for the created entry to be propagated to the other node.
+		await dhtSync([alice.player, bob.player], alice.player.cells[0].cell_id[0]);
 
-    // Alice updates the Note again
-    contentUpdate = await sampleNote(alice.store.client);
+		// Bob gets the updated Note
+		const readUpdatedOutput0 = await toPromise(
+			bob.store.notes.get(note.actionHash).latestVersion,
+		);
+		assert.equal(contentUpdate.title, readUpdatedOutput0.doc.title);
+		assert.equal(contentUpdate.body, readUpdatedOutput0.doc.body);
 
-    updatedNote = await alice.store.client.updateNote(originalActionHash, updatedNote.actionHash, contentUpdate);
-    assert.ok(updatedNote);
+		// Alice updates the Note again
+		contentUpdate = Automerge.change<Note>(latestVersion.doc, doc => {
+			doc.title = 'hey2';
+		});
 
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0],
-    );
+		updatedNote = await alice.store.client.updateNote(
+			originalActionHash,
+			[updatedNote.actionHash],
+			Automerge.save(contentUpdate),
+		);
+		assert.ok(updatedNote);
 
-    // Bob gets the updated Note
-    const readUpdatedOutput1: EntryRecord<Note> = await toPromise(
-      bob.store.notes.get(originalActionHash).latestVersion,
-    );
-    assert.deepEqual(contentUpdate, cleanNodeDecoding(readUpdatedOutput1.entry));
-  });
+		// Wait for the created entry to be propagated to the other node.
+		await dhtSync([alice.player, bob.player], alice.player.cells[0].cell_id[0]);
+
+		// Bob gets the updated Note
+		const readUpdatedOutput1 = await toPromise(
+			bob.store.notes.get(originalActionHash).latestVersion,
+		);
+		assert.equal(contentUpdate.title, readUpdatedOutput1.doc.title);
+		assert.equal(contentUpdate.body, readUpdatedOutput1.doc.body);
+	});
 });
 
-test("create and delete Note", async () => {
-  await runScenario(async scenario => {
-    const [alice, bob] = await setup(scenario);
+test('create and delete Note', async () => {
+	await runScenario(async scenario => {
+		const [alice, bob] = await setup(scenario);
 
-    // Alice creates a Note
-    const note: EntryRecord<Note> = await alice.store.client.createNote(await sampleNote(alice.store.client));
-    assert.ok(note);
+		// Alice creates a Note
+		const note: EntryRecord<Note> = await alice.store.client.createNote(
+			await sampleNote(alice.store.client),
+		);
+		assert.ok(note);
 
-    // Alice deletes the Note
-    const deleteActionHash = await alice.store.client.deleteNote(note.actionHash);
-    assert.ok(deleteActionHash);
+		// Alice deletes the Note
+		const deleteActionHash = await alice.store.client.deleteNote(
+			note.actionHash,
+		);
+		assert.ok(deleteActionHash);
 
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0],
-    );
+		// Wait for the created entry to be propagated to the other node.
+		await dhtSync([alice.player, bob.player], alice.player.cells[0].cell_id[0]);
 
-    // Bob tries to get the deleted Note
-    const deletes: Array<SignedActionHashed<Delete>> = await toPromise(bob.store.notes.get(note.actionHash).deletes);
-    assert.equal(deletes.length, 1);
-  });
+		// Bob tries to get the deleted Note
+		const deletes: Array<SignedActionHashed<Delete>> = await toPromise(
+			bob.store.notes.get(note.actionHash).deletes,
+		);
+		assert.equal(deletes.length, 1);
+	});
 });
--- ui/src/mocks.ts
+++ ui/src/mocks.ts
@@ -1,151 +1,186 @@
-import { Note } from "./types.js";
+import {
+	AgentPubKeyMap,
+	HashType,
+	HoloHashMap,
+	ZomeMock,
+	decodeEntry,
+	fakeCreateAction,
+	fakeDeleteEntry,
+	fakeEntry,
+	fakeRecord,
+	fakeUpdateEntry,
+	hash,
+	pickBy,
+} from '@darksoil-studio/holochain-utils';
+import {
+	ActionHash,
+	AgentPubKey,
+	AppClient,
+	Delete,
+	EntryHash,
+	Link,
+	NewEntryAction,
+	Record,
+	SignedActionHashed,
+	decodeHashFromBase64,
+	fakeActionHash,
+	fakeAgentPubKey,
+	fakeDnaHash,
+	fakeEntryHash,
+} from '@holochain/client';
 
-import {
-  AgentPubKeyMap,
-  decodeEntry,
-  fakeCreateAction,
-  fakeDeleteEntry,
-  fakeEntry,
-  fakeRecord,
-  fakeUpdateEntry,
-  hash,
-  HashType,
-  HoloHashMap,
-  pickBy,
-  ZomeMock,
-} from "@darksoil-studio/holochain-utils";
-import {
-  ActionHash,
-  AgentPubKey,
-  AppClient,
-  decodeHashFromBase64,
-  Delete,
-  EntryHash,
-  fakeActionHash,
-  fakeAgentPubKey,
-  fakeDnaHash,
-  fakeEntryHash,
-  Link,
-  NewEntryAction,
-  Record,
-  SignedActionHashed,
-} from "@holochain/client";
-import { NotesClient } from "./notes-client.js";
+import { NotesClient } from './notes-client.js';
+import { Note } from './types.js';
 
 export class NotesZomeMock extends ZomeMock implements AppClient {
-  constructor(
-    myPubKey?: AgentPubKey,
-  ) {
-    super("notes_test", "notes", "notes_test_app", myPubKey);
-  }
-  /** Note */
-  notes = new HoloHashMap<ActionHash, {
-    deletes: Array<SignedActionHashed<Delete>>;
-    revisions: Array<Record>;
-  }>();
+	constructor(myPubKey?: AgentPubKey) {
+		super('notes_test', 'notes', 'notes_test_app', myPubKey);
+	}
+	/** Note */
+	notes = new HoloHashMap<
+		ActionHash,
+		{
+			deletes: Array<SignedActionHashed<Delete>>;
+			revisions: Array<Record>;
+		}
+	>();
 
-  async create_note(note: Note): Promise<Record> {
-    const entryHash = hash(note, HashType.ENTRY);
-    const record = await fakeRecord(await fakeCreateAction(entryHash), fakeEntry(note));
+	async create_note(note: Note): Promise<Record> {
+		const entryHash = hash(note, HashType.ENTRY);
+		const record = await fakeRecord(
+			await fakeCreateAction(entryHash),
+			fakeEntry(note),
+		);
 
-    this.notes.set(record.signed_action.hashed.hash, {
-      deletes: [],
-      revisions: [record],
-    });
+		this.notes.set(record.signed_action.hashed.hash, {
+			deletes: [],
+			revisions: [record],
+		});
 
-    return record;
-  }
+		return record;
+	}
 
-  async get_latest_note(noteHash: ActionHash): Promise<Record | undefined> {
-    const note = this.notes.get(noteHash);
-    return note ? note.revisions[note.revisions.length - 1] : undefined;
-  }
+	async get_latest_note(noteHash: ActionHash): Promise<Record | undefined> {
+		const note = this.notes.get(noteHash);
+		return note ? note.revisions[note.revisions.length - 1] : undefined;
+	}
 
-  async get_all_revisions_for_note(noteHash: ActionHash): Promise<Record[] | undefined> {
-    const note = this.notes.get(noteHash);
-    return note ? note.revisions : undefined;
-  }
+	async get_all_revisions_for_note(
+		noteHash: ActionHash,
+	): Promise<Record[] | undefined> {
+		const note = this.notes.get(noteHash);
+		return note ? note.revisions : undefined;
+	}
 
-  async get_original_note(noteHash: ActionHash): Promise<Record | undefined> {
-    const note = this.notes.get(noteHash);
-    return note ? note.revisions[0] : undefined;
-  }
+	async get_original_note(noteHash: ActionHash): Promise<Record | undefined> {
+		const note = this.notes.get(noteHash);
+		return note ? note.revisions[0] : undefined;
+	}
 
-  async get_all_deletes_for_note(noteHash: ActionHash): Promise<Array<SignedActionHashed<Delete>> | undefined> {
-    const note = this.notes.get(noteHash);
-    return note ? note.deletes : undefined;
-  }
+	async get_all_deletes_for_note(
+		noteHash: ActionHash,
+	): Promise<Array<SignedActionHashed<Delete>> | undefined> {
+		const note = this.notes.get(noteHash);
+		return note ? note.deletes : undefined;
+	}
 
-  async get_oldest_delete_for_note(noteHash: ActionHash): Promise<SignedActionHashed<Delete> | undefined> {
-    const note = this.notes.get(noteHash);
-    return note ? note.deletes[0] : undefined;
-  }
-  async delete_note(original_note_hash: ActionHash): Promise<ActionHash> {
-    const record = await fakeRecord(await fakeDeleteEntry(original_note_hash));
+	async get_oldest_delete_for_note(
+		noteHash: ActionHash,
+	): Promise<SignedActionHashed<Delete> | undefined> {
+		const note = this.notes.get(noteHash);
+		return note ? note.deletes[0] : undefined;
+	}
+	async delete_note(original_note_hash: ActionHash): Promise<ActionHash> {
+		const record = await fakeRecord(await fakeDeleteEntry(original_note_hash));
 
-    this.notes.get(original_note_hash).deletes.push(record.signed_action as SignedActionHashed<Delete>);
+		this.notes
+			.get(original_note_hash)
+			.deletes.push(record.signed_action as SignedActionHashed<Delete>);
 
-    return record.signed_action.hashed.hash;
-  }
+		return record.signed_action.hashed.hash;
+	}
 
-  async update_note(
-    input: { original_note_hash: ActionHash; previous_note_hash: ActionHash; updated_note: Note },
-  ): Promise<Record> {
-    const record = await fakeRecord(
-      await fakeUpdateEntry(input.previous_note_hash, undefined, undefined, fakeEntry(input.updated_note)),
-      fakeEntry(input.updated_note),
-    );
+	async update_note(input: {
+		original_note_hash: ActionHash;
+		previous_note_hash: ActionHash;
+		updated_note: Note;
+	}): Promise<Record> {
+		const record = await fakeRecord(
+			await fakeUpdateEntry(
+				input.previous_note_hash,
+				undefined,
+				undefined,
+				fakeEntry(input.updated_note),
+			),
+			fakeEntry(input.updated_note),
+		);
 
-    this.notes.get(input.original_note_hash).revisions.push(record);
+		this.notes.get(input.original_note_hash).revisions.push(record);
 
-    const note = input.updated_note;
+		const note = input.updated_note;
 
-    return record;
-  }
+		return record;
+	}
 
-  async get_all_notes(): Promise<Array<Link>> {
-    const records: Record[] = Array.from(this.notes.values()).map(r => r.revisions[r.revisions.length - 1]);
-    const base = await fakeEntryHash();
-    return Promise.all(records.map(async record => ({
-      base,
-      target: record.signed_action.hashed.hash,
-      author: record.signed_action.hashed.content.author,
-      timestamp: record.signed_action.hashed.content.timestamp,
-      zome_index: 0,
-      link_type: 0,
-      tag: new Uint8Array(),
-      create_link_hash: await fakeActionHash(),
-    })));
-  }
+	async get_all_notes(): Promise<Array<Link>> {
+		const records: Record[] = Array.from(this.notes.values()).map(
+			r => r.revisions[r.revisions.length - 1],
+		);
+		const base = await fakeEntryHash();
+		return Promise.all(
+			records.map(async record => ({
+				base,
+				target: record.signed_action.hashed.hash,
+				author: record.signed_action.hashed.content.author,
+				timestamp: record.signed_action.hashed.content.timestamp,
+				zome_index: 0,
+				link_type: 0,
+				tag: new Uint8Array(),
+				create_link_hash: await fakeActionHash(),
+			})),
+		);
+	}
 
-  /** Incremental Changes for Note */
-  incrementalChangesForNote = new HoloHashMap<ActionHash, Link[]>();
+	/** Incremental Changes for Note */
+	incrementalChangesForNote = new HoloHashMap<ActionHash, Link[]>();
 
-  async get_incremental_changes_for_note(noteHash: ActionHash): Promise<Array<Link>> {
-    return this.incrementalChangesForNote.get(noteHash) || [];
-  }
+	async get_incremental_changes_for_note(
+		noteHash: ActionHash,
+	): Promise<Array<Link>> {
+		return this.incrementalChangesForNote.get(noteHash) || [];
+	}
 
-  async add_incremental_changes_for_note(input: { note_hash: ActionHash; incremental_changes: string }): Promise<void> {
-    const existing = this.incrementalChangesForNote.get(input.note_hash) || [];
-    this.incrementalChangesForNote.set(input.note_hash, [...existing, {
-      base: input.note_hash,
-      target: input.note_hash,
-      author: this.myPubKey,
-      timestamp: Date.now() * 1000,
-      zome_index: 0,
-      link_type: 0,
-      tag: new TextEncoder().encode(input.incremental_changes),
-      create_link_hash: await fakeActionHash(),
-    }]);
-  }
+	async add_incremental_changes_for_note(input: {
+		note_hash: ActionHash;
+		incremental_changes: string;
+	}): Promise<void> {
+		const existing = this.incrementalChangesForNote.get(input.note_hash) || [];
+		this.incrementalChangesForNote.set(input.note_hash, [
+			...existing,
+			{
+				base: input.note_hash,
+				target: input.note_hash,
+				author: this.myPubKey,
+				timestamp: Date.now() * 1000,
+				zome_index: 0,
+				link_type: 0,
+				tag: new TextEncoder().encode(input.incremental_changes),
+				create_link_hash: await fakeActionHash(),
+			},
+		]);
+	}
 }
 
-export async function sampleNote(client: NotesClient, partialNote: Partial<Note> = {}): Promise<Note> {
-  return {
-    ...{
-      title: "Lorem ipsum 2",
-      body: "Lorem ipsum 2",
-    },
-    ...partialNote,
-  };
+export async function sampleNote(
+	client: NotesClient,
+	partialNote: Partial<Note> = {},
+): Promise<Note> {
+	return {
+		...{
+			title: 'Lorem ipsum 2',
+			body: 'Lorem ipsum 2',
+			images_hashes: [],
+		},
+		...partialNote,
+	};
 }
